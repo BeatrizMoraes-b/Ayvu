6fb2b99724b3b215e9e17726a005bdbaedfe2647
Beatriz de Moraes <beatrizeft@gmail.com>
2026-08-16 15:27:05 -0300

    alterei o config.php
    adicionei o mysqli_report para evitar mensagens de erro que contenham informações dos usuários na tela e o utf8 para evitar problemas com caracteres.

--- AYVU/config.php
+++ AYVU/config.php
@@ -1,17 +1,27 @@
 <?php
-$dbHost = 'LocalHost';
+
+$dbHost     = 'localhost';
 $dbUsername = 'root';
 $dbPassword = '';
-$dbName = 'db_ayvu';
+$dbName     = 'db_ayvu';
 
-$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 
-if($conexao->connect_error)
-    {
-        echo "Erro";
+mysqli_report(MYSQLI_REPORT_OFF);
+
+try {
+    
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+   
+    if ($conexao->connect_error) {
+        throw new Exception("Falha na conexão com o banco de dados.");
     }
-    else
-    {
-        echo "Conexão efetuada com sucesso!";
-    }
+
+   
+    $conexao->set_charset("utf8mb4");
+
+} catch (Exception $e) {
+
+    die("Ocorreu um erro ao conectar ao sistema. Tente novamente mais tarde.");
+}
 ?>
