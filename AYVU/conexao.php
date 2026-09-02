92324fcfb051aa132721c8d38ef285467262431c
Beatriz de Moraes <beatrizeft@gmail.com>
2026-09-02 15:15:20 -0300

    tratativa de erros (teste)

--- AYVU/conexao.php
+++ AYVU/conexao.php
@@ -11,6 +11,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email']; 
     $senha = $_POST['senha'];
 
+
+// Esse bloco serve para a captação de erros de inserção do usuário
+$erros = [];
+
+    if (empty($nome)) {
+        $erros[] = "O campo Nome é obrigatório.";
+    }
+    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $erros[] = "Informe um e-mail válido.";
+    }
+    if (strlen($senha) < 6) {
+        $erros[] = "A senha deve ter pelo menos 6 caracteres.";
+    }
+
+
+    if (!empty($erros)) {
+        foreach ($erros as $erro) {
+            echo "<p style='color: red;'>$erro</p>";
+        }
+        echo "<a href='javascript:history.back()'>Voltar e corrigir</a>";
+        exit; 
+    }
+    // ele acaba aqui  
+
     $senha = password_hash($senha, PASSWORD_DEFAULT);
 
    
