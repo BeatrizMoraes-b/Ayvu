6d550b2c64b9b3e80ce8444d95b72c932d4b1b55
Beatriz de Moraes <beatrizeft@gmail.com>
2026-09-02 15:49:17 -0300

    Update bd_ayvu.sql
    fiz alterações no banco para conexão com a página

--- AYVU/bd_ayvu.sql
+++ AYVU/bd_ayvu.sql
@@ -1,11 +1,15 @@
 create database ayvu;
 use ayvu;
 
-create table usuarios(
-idusuario BIGINT NOT NULL AUTO_INCREMENT,
-nome VARCHAR(100) NOT NULL ,
-genero VARCHAR(20) NOT NULL ,
-senha VARCHAR(255) NOT NULL ,
-email VARCHAR(255) NOT NULL UNIQUE,
-PRIMARY KEY(idusuario));
+drop table usuarios; 
+CREATE TABLE usuarios (
+    idusuario BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    sobrenome VARCHAR(100) NOT NULL,
+    genero VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    PRIMARY KEY(idusuario)
+);
 
+select * from usuarios;
