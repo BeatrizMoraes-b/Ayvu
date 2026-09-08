08a1042ca02c848140a1df4717b32f5d422be5d9
Beatriz de Moraes <beatrizeft@gmail.com>
2026-09-08 12:58:56 -0300

    consertei um erro de sintax

--- AYVU/extensao/js/background.js
+++ AYVU/extensao/js/background.js
@@ -22,7 +22,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.acao === 'OBTER_STATUS') {
     sendResponse({ status: 'Service Worker ativo' });
   } else {
-    sendResponse ({ status: 'Ação não processada pelo background' };)
+    sendResponse ({ status: 'Ação não processada pelo background' });
   }
 
 });
